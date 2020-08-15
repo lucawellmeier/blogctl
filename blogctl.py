8b42e32180f856557a00943312125a0a128f1bae
Luca Wellmeier <luca_wellmeier@gmx.de>
2020-08-15 15:23:29 +0200

    rename the command in help to octoblog #6

--- blogctl.py
+++ blogctl.py
@@ -12,8 +12,8 @@ from publish_cmd import PublishCommand
 
 class CLI:
     def __init__(self):
-        parser = argparse.ArgumentParser(prog='blogctl', 
-                epilog='use "blogctl <command> -h" for details on a specific command')
+        parser = argparse.ArgumentParser(prog='octoblog', 
+                epilog='use "octoblog <command> -h" for details on a specific command')
         parser.add_argument('command', 
                 help='available commands: init, clone, status, save, preview, publish')
         args = parser.parse_args(sys.argv[1:2])
@@ -23,7 +23,7 @@ class CLI:
         getattr(self, args.command)(sys.argv[2:])
 
     def init(self, args):
-        parser = argparse.ArgumentParser(prog='blogctl init', 
+        parser = argparse.ArgumentParser(prog='octoblog init', 
                 description='initialize new blog with dummy files')
         parser.add_argument('remote', 
                 help='link to your remote GitHub Pages repo')
@@ -31,33 +31,33 @@ class CLI:
         InitCommand(initArgs.remote)
 
     def clone(self, args):
-        parser = argparse.ArgumentParser(prog='blogctl clone', 
-                description='clone existing GitHub Pages blogctl repo')
+        parser = argparse.ArgumentParser(prog='octoblog clone', 
+                description='clone existing GitHub Pages octoblog repo')
         parser.add_argument('remote', 
                 help='link to your repo')
         cloneArgs = parser.parse_args(args)
         CloneCommand(cloneArgs.remote)
 
     def save(self, args):
-        parser = argparse.ArgumentParser(prog='blogctl save', 
+        parser = argparse.ArgumentParser(prog='octoblog save', 
                 description='save changes to be published later')
         parser.parse_args(args)
         SaveCommand()
 
     def preview(self, args):
-        parser = argparse.ArgumentParser(prog='blogctl preview', 
+        parser = argparse.ArgumentParser(prog='octoblog preview', 
                 description='generate a preview of the current state')
         parser.parse_args(args)
         PreviewCommand()
 
     def publish(self, args):
-        parser = argparse.ArgumentParser(prog='blogctl publish', 
+        parser = argparse.ArgumentParser(prog='octoblog publish', 
                 description='publish current state to the web')
         parser.parse_args(args)
         PublishCommand()
 
     def status(self, args):
-        parser = argparse.ArgumentParser(prog='blogctl status', 
+        parser = argparse.ArgumentParser(prog='octoblog status', 
                 description='list unsaved and unpublished files')
         parser.parse_args(args)
 
